3d27c71e2b98def7dca5ac6bf72f2da65029b6d1
Dylan DE SOUSA <dds1991@hotmail.fr>
2022-09-16 16:51:28 +0200

    Update countWord.js

--- workshops/01-easy/word-count/src/countWord.js
+++ workshops/01-easy/word-count/src/countWord.js
@@ -1,10 +1,23 @@
 'use strict';
 
-function countWords(string) {
+function countWords(sentence) {
+  const words = sentence.split(' ');
+  let result = {};
+  
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i].toLowerCase();
 
+    if (!(word in result)) {
+      result[word] = 0;
+    }
+
+    result[word] += 1;
+  }
+
+  return result;
 }
 
 // It should log -> { "are": 1, "you": 1, "the": 1, "queen": 1 }
 console.log(
-  countWords('Are you the queen ?'),
+  countWords('Are are aRe you the queen ?'),
 );
