b45ba2f51abfa7a28fccf90962321500978a2163
Dylan DE SOUSA <dds1991@hotmail.fr>
2022-09-16 16:50:45 +0200

    Update upperCase.js

--- workshops/01-easy/upper-case/src/upperCase.js
+++ workshops/01-easy/upper-case/src/upperCase.js
@@ -1,12 +1,24 @@
 'use strict';
 
 function upperCase(string) {
+  let result = '';
 
+  for (let i = 0; i < string.length; i++) {
+    const code = string.charCodeAt(i);
+
+    if (code >= 97 && code <= 122) {
+      result += String.fromCharCode(code - 32);
+    } else {
+      result += string[i];
+    }
+  }
+
+  return result;
 }
 
 // It should log -> UHOO
 console.log(
-  upperCase('uhoo'),
+  upperCase('uhoo ?'),
 );
 
 // It should log ->
