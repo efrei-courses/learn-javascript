a54967eb38f0ffecc6c4883785ff63fb01a9eb8a
Dylan DE SOUSA <dds1991@hotmail.fr>
2022-09-16 16:48:03 +0200

    Update lowerCase.js

--- workshops/01-easy/lower-case/src/lowerCase.js
+++ workshops/01-easy/lower-case/src/lowerCase.js
@@ -1,7 +1,19 @@
 'use strict';
 
 function lowerCase(string) {
+  let result = '';
 
+  for (let i = 0; i < string.length; i++) {
+    const code = string.charCodeAt(i);
+
+    if (code >= 65 && code <= 90) {
+      result += String.fromCharCode(code + 32);
+    } else {
+      result += string[i];
+    }
+  }
+
+  return result;
 }
 
 // It should log -> "tiens tiens tiens ..."
